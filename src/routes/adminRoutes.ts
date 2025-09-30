c7e6aa49a6cbe38ae5bb54c9ddf48f04423075ad
Alan Ng <nkk991026@gmail.com>
2025-09-30 17:44:44 +0800

    slight modification on admin routes

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -11,20 +11,20 @@ const router = express.Router();
 // User Routes (Admin access only)
 router.get("/users/all-users", isAuthenticated, isAdmin, viewAllUsers);
 router.get("/users/search-users", isAuthenticated, isAdmin, searchUsers);
-router.get("/users/user/profile/:id", isAuthenticated, isAdmin, viewUserProfile);
-router.patch("/users/user/profile/:id", isAuthenticated, isAdmin, editUserProfile);
-router.post("/users/user/profile/:id/profile-image", isAuthenticated, isAdmin, upload.single("profile_image"), uploadUserProfileImage);
-router.delete("/users/user/profile/:id/profile-image", isAuthenticated, isAdmin, deleteUserProfileImage);
-router.delete("/users/user/profile/:id", isAuthenticated, isAdmin, deleteUserAccount);
+router.get("/users/user/profile/:userId", isAuthenticated, isAdmin, viewUserProfile);
+router.patch("/users/user/profile/:userId", isAuthenticated, isAdmin, editUserProfile);
+router.post("/users/user/profile/:userId/profile-image", isAuthenticated, isAdmin, upload.single("profile_image"), uploadUserProfileImage);
+router.delete("/users/user/profile/:userId/profile-image", isAuthenticated, isAdmin, deleteUserProfileImage);
+router.delete("/users/user/profile/:userId", isAuthenticated, isAdmin, deleteUserAccount);
 
 // Post Routes (Admin access only)
 router.get("/posts/all-posts", isAuthenticated, isAdmin, viewAllPosts);
-router.get("/posts/post/:id", isAuthenticated, isAdmin, viewPost);
+router.get("/posts/post/:postId", isAuthenticated, isAdmin, viewPost);
 router.get("/posts/search-posts", isAuthenticated, isAdmin, searchPosts);
-router.delete("/posts/post/:id", isAuthenticated, isAdmin, deleteUserPost);
+router.delete("/posts/post/:postId", isAuthenticated, isAdmin, deleteUserPost);
 
 // Comment Routes (Admin access only)
 router.get("/comments/all-comments", isAuthenticated, isAdmin, viewComments);
-router.delete("/comments/comment/:id", isAuthenticated, isAdmin, deleteUserComment);
+router.delete("/comments/comment/:commentId", isAuthenticated, isAdmin, deleteUserComment);
 
 export default router;
